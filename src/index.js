f755af8a319a84396f3cb83e6f26682579667eb3
Marcos Teodoro <marcosvinici_us@live.com>
2020-01-16 10:13:19 -0300

    Application CORS access allow

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -9,6 +10,7 @@ mongoose.connect('mongodb://root:toor@localhost:27017/devradar?authSource=admin'
   useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json()); // Enable usage of JSON on request body (POST, PUT)
 app.use(routes);
 
