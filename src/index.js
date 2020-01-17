a46b639710b713a4cc47f237d6a4cae03deb8d96
Marcos Teodoro <marcosvinici_us@live.com>
2020-01-17 16:11:52 -0300

    Socket.io setup and usage

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
+
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb://root:toor@localhost:27017/devradar?authSource=admin', {
   useNewUrlParser: true,
@@ -14,4 +20,4 @@ app.use(cors());
 app.use(express.json()); // Enable usage of JSON on request body (POST, PUT)
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
